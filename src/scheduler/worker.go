5d379c2f6ce9219c5747a77d262c5861bb1cc63f
chrisdev <johnnytriple33@gmail.com>
2021-02-14 19:07:08 -0800

    Added basic structure for messages.

--- src/scheduler/worker.go
+++ src/scheduler/worker.go
@@ -1,1 +1,19 @@
 package scheduler
+
+func runJob(msg chan<- Message) {
+
+}
+
+type Message struct {
+	msgType messageType
+	msgData string
+}
+
+type messageType int
+
+const (
+	analyze = iota
+	suspend
+	resume
+	stop
+)
