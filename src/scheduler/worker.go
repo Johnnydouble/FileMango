204eddd7c788f87f9fc8b57c983288b776901e08
Lexicon9 <lexicon99999@gmail.com>
2021-02-15 18:01:05 -0800

    added marshalMessage function to turn message struct into json string

--- src/scheduler/worker.go
+++ src/scheduler/worker.go
@@ -1,10 +1,17 @@
 package scheduler
 
-func runJob(msg chan<- Message) {
+import (
+	"encoding/json"
+	"fmt"
+)
 
+var testMsg = message{msgType: analyze, msgData: "/home/melkor/Pictures/aur-plugin.png"}
+
+func runJob(msg chan<- message) {
+	marshalMessage(testMsg) //test Marshal function
 }
 
-type Message struct {
+type message struct {
 	msgType messageType
 	msgData string
 }
@@ -17,3 +24,13 @@ const (
 	resume
 	stop
 )
+
+func marshalMessage(msg message) string {
+	message, err := json.Marshal(msg)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	fmt.Println(string(message))
+	return string(message)
+}
