ae7b56cb254e64bbbce72c427fc21ae508e23b9e
Lexicon9 <lexicon99999@gmail.com>
2021-02-21 19:46:34 -0800

    added writing of attributes to writeAttributes

--- src/scheduler/writeAttrs.go
+++ src/scheduler/writeAttrs.go
@@ -1,8 +1,14 @@
 package scheduler
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/pkg/xattr"
+)
 
 func writeAttributes(msg message) {
+	for _, pair := range msg.Output.Pairs {
+		_ = xattr.Set(msg.Input.Data, pair.Key, []byte(pair.Value))
+	}
 	fmt.Print("WROTE: ")
 	fmt.Print(msg)
 	fmt.Println(" TO ATTRIBUTES")
